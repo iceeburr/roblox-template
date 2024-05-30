8d4caecc4e83b9b5923ee1ab0a2a8195838db694
iceeburr <iceeburr@gmail.com>
2024-05-30 18:15:58 +0300

    added stylua to and improved code quality checks script

--- scripts/cq.sh
+++ scripts/cq.sh
@@ -5,10 +5,17 @@ set -euo errtrace
 error_handle() {
     if [[ ${BASH_COMMAND} == *"luau-lsp analyze"* ]]; then
         echo $1
+        stylua --check --verify src
         echo "Failed code quality checks!"
+        exit 1
+    elif [[ ${BASH_COMMAND} == *"stylua"* ]]; then
+        echo $1
+        echo "Stylua formatting failed!"
+        exit 1
     else
         echo "error: $1 at line ${BASH_LINENO[0]}"
         echo "Are you sure you ran the install.sh script?"
+        exit 1
     fi
 }
 trap 'error_handle "$BASH_COMMAND"' ERR
@@ -20,6 +27,11 @@ cd "$SCRIPT_DIR"
 # Analyze the project.
 curl -L "https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua" > "globalTypes.d.lua"
 rojo sourcemap default.project.json -o sourcemap.json
-luau-lsp analyze --sourcemap sourcemap.json --definitions globalTypes.d.lua src tests
+luau-lsp analyze --sourcemap sourcemap.json --definitions globalTypes.d.lua \
+--ignore Packages/**/*.lua --ignore Packages/**/*.lua \
+--ignore CustomPackages/**/*.lua --ignore CustomPackages/**/*.luau \
+src
+
+stylua --check --verify src
 
 echo "Successfully ran code quality checks."
